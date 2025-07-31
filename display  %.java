48a0b6fdf3d1bf77672c178ab250c2c76c5e44e4
Manikandan0714 <127004555+Manikandan0714@users.noreply.github.com>
2025-07-31 12:17:07 +0530

    Create display  %.java
    Write a c program to display the %

---
+++ display  %.java
@@ -0,0 +1,26 @@
+/*
+Write a c program to display the %
+Input Format
+Nill
+
+Constraints
+Nill
+
+Output Format
+%%
+
+Sample Output 0
+%%
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("%%");
+    }
+}

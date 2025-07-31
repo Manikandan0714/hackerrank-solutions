272e4081dc5990c258bfeaaeb6f596bd66c8ca0e
Manikandan0714 <127004555+Manikandan0714@users.noreply.github.com>
2025-07-31 12:21:21 +0530

    Create Print the following output.java
    Print the following output:

---
+++ Print the following output.java
@@ -0,0 +1,28 @@
+/*
+Print the following output:
+Success is when your "signature" becomes "autograph"
+
+Input Format
+Nill
+
+Constraints
+Nill
+
+Output Format
+Display the given output
+
+Sample Output 0
+Success is when your "signature" becomes "autograph"
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Success is when your \"signature\" becomes \"autograph\"");
+    }
+}

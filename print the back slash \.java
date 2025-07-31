45372dfe7f14243aa8b050feac5ad7f6a2a2be7f
Manikandan0714 <127004555+Manikandan0714@users.noreply.github.com>
2025-07-31 11:32:28 +0530

    Create print the back slash \.java
    Write a c program to print the back slash \

---
+++ print the back slash \.java
@@ -0,0 +1,29 @@
+/*
+Write a c program to print the back slash \
+Input Format
+Nill
+
+Constraints
+Nill
+
+Output Format
+Display the \
+
+Sample Input 0
+\
+
+Sample Output 0
+\
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("\\");
+        }
+}

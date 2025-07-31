a83ee3899b012397b98cb946e4ac523fd1eabe24
Manikandan0714 <127004555+Manikandan0714@users.noreply.github.com>
2025-07-31 12:19:17 +0530

    Create display the output.java
    Write a program to display the output

---
+++ display the output.java
@@ -0,0 +1,26 @@
+/*
+Write a program to display the output
+Input Format
+Nill
+
+Constraints
+Nill
+
+Output Format
+Display the string
+
+Sample Output 0
+Let's learn 'C' together with MySlate team 
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Let's learn 'C' together with MySlate team");
+    }
+}

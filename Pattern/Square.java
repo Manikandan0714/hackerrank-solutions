54298d8ded94edc0d1501cd609050b85da08113d
Manikandan0714 <127004555+Manikandan0714@users.noreply.github.com>
2025-08-01 15:03:24 +0530

    Create Square.java
    ****
    ****
    ****
    ****

---
+++ Pattern/Square.java
@@ -0,0 +1,40 @@
+/*
+Write a progam to display the following pattern.
+Input Format
+Integer
+
+Constraints
+input>0
+
+Output Format
+Star Pattern
+
+Sample Input 0
+4
+
+Sample Output 0
+****
+****
+****
+****
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r;
+        int n = sc.nextInt();
+        for (int i = 1; i<=n ; i++){
+            for (int j=1; j<=n ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

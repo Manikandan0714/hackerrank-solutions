22e5e61ee262c706bf38803589825a7d72caa32f
Manikandan0714 <127004555+Manikandan0714@users.noreply.github.com>
2025-08-01 15:14:26 +0530

    Create Rhombus.java
        *****
       *****
      *****
     *****
    *****

---
+++ Pattern/Rhombus.java
@@ -0,0 +1,50 @@
+/*
+Print the following pattern
+
+Input Format
+
+Integer
+
+Constraints
+
+input>0
+
+Output Format
+
+Star pattern
+
+Sample Input 0
+
+5
+Sample Output 0
+
+    *****
+   *****
+  *****
+ *****
+*****
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= n; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

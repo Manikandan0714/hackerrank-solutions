025fde17c1650ab21589421c2c5c551f4fc1653f
Manikandan0714 <127004555+Manikandan0714@users.noreply.github.com>
2025-08-02 21:29:41 +0530

    Create X + Hollow Square.java
    *******
    **   **
    * * * *
    *  *  *
    * * * *
    **   **
    *******

---
+++ Pattern/X + Hollow Square.java
@@ -0,0 +1,46 @@
+/*
+Write a program to display the following pattern
+Input Format
+Integer
+
+Constraints
+input>0
+
+Output Format
+Star pattern
+
+Sample Input 0
+4
+
+Sample Output 0
+*******
+**   **
+* * * *
+*  *  *
+* * * *
+**   **
+*******
+*/
+import java.util.Scanner;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int size = 2 * n - 1;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 || i == size - 1 || j == 0 || j == size - 1 || i == j || i + j == size - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}

aca379c90240aabda4761346b6a5049767f3d06e
S Manikandan <manikandansethuraman007@gmail.com>
2025-08-28 22:09:21 +0530

    Create infinite star.java

---
+++ Pattern Lvl 2/infinite star.java
@@ -0,0 +1,48 @@
+/*
+Print the following pattern
+
+Input Format
+Accept an integer as an input
+
+Constraints
+NIL
+
+Output Format
+Pattern Format
+
+Sample Input 0
+4
+Sample Output 0
+*      *
+**    **
+***  ***
+********
+********
+***  ***
+**    **
+*      *
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // Top half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) System.out.print("*");
+            for (int j = 1; j <= 2 * (n - i); j++) System.out.print(" ");
+            for (int j = 1; j <= i; j++) System.out.print("*");
+            System.out.println();
+        }
+
+        // Bottom half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) System.out.print("*");
+            for (int j = 1; j <= 2 * (n - i); j++) System.out.print(" ");
+            for (int j = 1; j <= i; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
